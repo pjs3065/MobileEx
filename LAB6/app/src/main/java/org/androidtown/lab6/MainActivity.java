ad792b24f478a498982c81ff90cc24d5bcbf452a
parkjaeseong <tmakdlfwotjd@naver.com>
2017-05-25 13:56:16 +0900

    Lab6(update)

--- LAB6/app/src/main/java/org/androidtown/lab6/MainActivity.java
+++ LAB6/app/src/main/java/org/androidtown/lab6/MainActivity.java
@@ -1,105 +1,93 @@
 package org.androidtown.lab6;
 
-import android.os.Environment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.ImageView;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-    //FilePath
-    public static final String filePath = "notes.txt";
-    //EditBox
-    private EditText txtUIData;
+    ImageView imageView1;
+    ImageView imageView2;
+    EditText editText;
+    Button button;
+    Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Get Contents by ID
-        txtUIData = (EditText) findViewById(R.id.txtData);
-        Button button1 = (Button) findViewById(R.id.button1);
-        Button button2 = (Button) findViewById(R.id.button2);
-        Button button3 = (Button) findViewById(R.id.button3);
-        Button button4 = (Button) findViewById(R.id.button4);
+        imageView1 = (ImageView) findViewById(R.id.imageView1);
+        imageView2 = (ImageView) findViewById(R.id.imageView2);
+        editText = (EditText) findViewById(R.id.editText);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
 
-        //Make Directory
-        File sdCard = Environment.getExternalStorageDirectory();
-        File directory = new File(sdCard.getAbsolutePath() + "/MyFiles");
-        directory.mkdir();
-
-        //Make File
-        final File file = new File(directory , filePath);
-
-        //Write File Button Event
-        button1.setOnClickListener(new Button.OnClickListener() {
-            public void onClick(View v) {
-                try {
-                    FileOutputStream out = new FileOutputStream(file);
-                    OutputStreamWriter osw = new OutputStreamWriter(out);
-
-                    osw.write(txtUIData.getText().toString());
-                    osw.close();
-                } catch (Throwable t) {
-                    Toast.makeText(getApplicationContext(), "Exception: " + t.toString(), Toast.LENGTH_SHORT).show();
-                }
-
-                Toast.makeText(getApplicationContext(), "Done Writing SD '" + filePath +" '", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        //Clear EditBox Button Event
-        button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                txtUIData.setText("");
+                DogThread thread1 = new DogThread(0);
+                thread1.start();
+                DogThread thread2 = new DogThread(1);
+                thread2.start();
             }
-        });
 
-        //Read File Button Event
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String read = new String();
-
-                try {
-                    FileInputStream in = new FileInputStream(file);
-                    InputStreamReader isr = new InputStreamReader(in);
-
-                    //Get Characters
-                    int res;
-                    while((res = isr.read()) != -1) {
-                        read += (char)res;
-                    }
-                    isr.close();
-
-                    //Print into EditBox
-                    txtUIData.setText(read);
-
-                }
-                catch (Throwable t) {
-                    Toast.makeText(getApplicationContext(), "Exception: " + t.toString(), Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-        //Finish Application Button Event
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-               finish();
-            }
         });
     }
+
+    class DogThread extends Thread {
+
+        int dogIndex;
+        int stateIndex;
+
+        ArrayList<Integer> images = new ArrayList<Integer>();
+
+        public DogThread(int index) {
+            dogIndex = index;
+            images.add(R.drawable.dog_eating);
+            images.add(R.drawable.dog_standing);
+            images.add(R.drawable.dog_study);
+
+
+        }
+
+        public void run() {
+            stateIndex = 0;
+            for (int i = 0; i < 9; i++) {
+                final String msg = "dog #" + dogIndex + " state: " + stateIndex;
+                handler.post(new Runnable() {
+                    public void run() {
+                        editText.append(msg + "\n");
+
+                        if (dogIndex == 0) {
+                            imageView1.setImageResource(images.get(stateIndex));
+                        } else if (dogIndex == 1) {
+                            imageView2.setImageResource(images.get(stateIndex));
+                        }
+                    }
+                });
+
+                try {
+                    int sleepTime = getRandomTime(500, 3000);
+                    Thread.sleep(sleepTime);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                stateIndex++;
+                if (stateIndex >= images.size()) {
+                    stateIndex = 0;
+                }
+            }
+        }
+
+        public int getRandomTime(int min, int max) {
+            return min + (int) (Math.random() * (max - min));
+        }
+    }
 }
+
 
